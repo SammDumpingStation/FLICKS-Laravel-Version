8fe32a236338d0fbf7259fe1726e8ea73a96c3b3
Samm Caagbay <sammcaag@gmail.com>
2024-07-04 18:37:26 +0800

    👍 Adding the global configuration in tailwind

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../public/css/Log-In/auth_portal.css">
-  <title> Landing Page</title>
-</head>
-<body>
-  <form action="" method="get">
+<x-auth>
+    <form action="" method="get">
     <h1 class="title"><span class="operation-type">Register</span> with FLICKS Today!</h1>
    <h2 class="title2">Log in to access your personalized dashboard and special offers!</h2>  {{-- Or Join us today and enjoy exclusive benefits and discounts! --}}
     <section class="option-section">
@@ -15,14 +7,14 @@
         <input id="have-account" class="radio" type="radio" name="options" value="Customer">
         <span class="custom-radio"></span>
         <img src="" alt="">
-        <label for="have-account">I'm a Customer</label>
+        <label for="have-account">Continue as Customer</label>
       </label>
 
       <label for="no-account" class="option">
         <input id="no-account" class="radio" type="radio" name="options" value="Admin">
         <span class="custom-radio"></span>
         <img src="" alt="">
-        <label for="no-account">I'm an Admin</label>
+        <label for="no-account">Continue as Guest</label>
       </label>
       <section class="portal-button">
         <button class="go-back" name="portal-button" value="cancel">
@@ -33,6 +25,4 @@
         </button>
       </section>
   </form>
-  </main>
-</body>
-</html>
+</x-auth>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "primary-black": "#212121", /* mainly-bg */
+                "secondary-black": "#171717", /* mainly navbar */
+                "tertiary-black": "#2F2F2F", /* mainly search bar */
+                "primary-white": "#ECECEC", /* 90% font */
+                "primary-grey": "#9B9B9B", /* 10% font */
+                "secondary-grey": "#383838", /* mainly border */
+                "primary-green": "#14A27F", /* mainly buttons */
+                "primary-yellow": "#f2ac42", //Edit Button
+                "primary-red": "#EA324C", //Delete button
+            },
+        },
     },
     plugins: [],
 };
